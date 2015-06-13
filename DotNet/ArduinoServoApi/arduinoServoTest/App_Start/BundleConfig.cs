e074495d36bd9f54ad5df74f717360d8347face2
Carrie <rankincarrieathena@outlook.com>
2015-06-13 13:52:53 -0400

    Miles' fixes

--- DotNet/ArduinoServoApi/arduinoServoTest/App_Start/BundleConfig.cs
+++ DotNet/ArduinoServoApi/arduinoServoTest/App_Start/BundleConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
-namespace _17steps {
+namespace glfledge {
     public class BundleConfig {
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles) {
--- DotNet/ArduinoServoApi/arduinoServoTest/App_Start/FilterConfig.cs
+++ DotNet/ArduinoServoApi/arduinoServoTest/App_Start/FilterConfig.cs
@@ -1,7 +1,8 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 
-namespace _17steps {
+namespace glfledge
+{
     public class FilterConfig {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
             filters.Add(new HandleErrorAttribute());
